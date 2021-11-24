92e57b3c4ef4c57d22ca6f97722e9be8fa10f47f
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-24 16:38:15 +0530

    Styling home page

---
+++ src/components/Section.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Section() {
+    return (
+        <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <Buttons>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                        Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg"></DownArrow>
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: url('/images/model-s.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12x;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+`
