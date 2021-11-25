d4bd3c9d469fae1eb31dcb5e3fc3ed08f3b56581
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-25 21:20:12 +0530

    Adding Animation to Home Pages

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
     return (
         <Wrap bgImage={backgroundImg}>
+            <Fade bottom>
             <ItemText>
                 <h1>{title}</h1>
                 <p>{description}</p>
             </ItemText>
+            </Fade>
             <Buttons>
+                <Fade bottom>
                 <ButtonGroup>
                     <LeftButton>
                         {leftBtnText}
@@ -20,6 +24,7 @@ function Section({ title, description, backgroundImg, leftBtnText, rightBtnText 
                         }
 
                 </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg"></DownArrow>
             </Buttons>
         </Wrap>
