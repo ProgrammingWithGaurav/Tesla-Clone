7c215658ae38981df7490da1af4fe114c45b3cc5
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-26 15:07:00 +0530

    fetch cars from redux store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
- // eslint-disable-next-line
- import { Counter } from './features/counter/Counter';
  import './App.css';
- // eslint-disable-next-line
 import Header from './components/Header';
 import Home from './components/Home';
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,37 +2,41 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg" alt="logo" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car, index) => (
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu onClick={() => setBurgerStatus(true)}/>
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
             </RightMenu>
             <BurgerNav show={burgerStatus}>
                 <CloseWrapper>
-                    <CustomClose onClick={() => setBurgerStatus(false)}/>
+                    <CustomClose onClick={() => setBurgerStatus(false)} />
                 </CloseWrapper>
+                {cars && cars.map((car, index) => (
+                <li key={index}><a href="#">{car}</a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
-                <li><a href="#">Trade-i</a></li>
+                <li><a href="#">Trade-in</a></li>
                 <li><a href="#">Cybertruck</a></li>
-                <li><a href="#">Roadaster</a></li>
-                <li><a href="#">Semi</a></li>
-                <li><a href="#">Charging</a></li>
-                <li><a href="#">Power</a></li>
+                <li><a href="#">Roadmaster</a></li>
+
             </BurgerNav>
         </Container>
     )
